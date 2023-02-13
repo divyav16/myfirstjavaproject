78a371a26d4b55c58b91e38e204d8503b8840f27
divya vandra <divyavandra@gmail.com>
2023-02-13 22:32:12 +0000

    myjavaprogramme

---
+++ FirstClass.java
@@ -0,0 +1,16 @@
+package firstp;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Tester");
+        System.out.println("Test");
+    }
+}
+
+//this is a single line comment
+/*this is a multiline comment
+so that can have e.g.1 2 or many lines
+ */
+
