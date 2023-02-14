de24cb9777f6185eb5a15a3b161a87bdc6cbe5d9
divyav16 <124738597+divyav16@users.noreply.github.com>
2023-02-14 18:31:30 +0000

    Add files via upload

---
+++ SecondClass.java
@@ -0,0 +1,8 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Java Project 1");
+        System.out.println("Project Java");
+        //this is my first java project
+
+    }
+}
