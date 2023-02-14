bcea472f3e976fec226e5ef09bf225ca538fc613
divyav16 <124738597+divyav16@users.noreply.github.com>
2023-02-14 18:32:25 +0000

    Add files via upload

---
+++ Class3.java
@@ -0,0 +1,8 @@
+public class Class3 {
+    /*this is practice session for java coding. have to use git and github*/
+
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("github");
+    }
+}
